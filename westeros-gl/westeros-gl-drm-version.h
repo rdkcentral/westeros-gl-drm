7bb6ad06f857b84542328176778a884e0c628fa5
sajnanazeer <sajnanazeerk@gmail.com>
2026-02-23 13:02:43 +0530

    Create westeros-gl-drm-version.h

---
+++ westeros-gl/westeros-gl-drm-version.h
@@ -0,0 +1,29 @@
+/*
+ * If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2018 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef _WST_GL_DRM_VERSION_H
+#define _WST_GL_DRM_VERSION_H
+
+#define WST_GL_DRM_VERSION_MAJOR (2)
+#define WST_GL_DRM_VERSION_MINOR (0)
+#define WST_GL_DRM_VERSION_PATCH (0)
+#define WST_GL_DRM_VERSION WST_GL_DRM_VERSION_MAJOR, WST_GL_DRM_VERSION_MINOR, WST_GL_DRM_VERSION_PATCH
+
+#define WST_GL_DRM_VERSION_FMT "%d.%02d.%02d"
+
+#endif
